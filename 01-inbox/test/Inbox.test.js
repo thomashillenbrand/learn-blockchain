2aae391752ee17c7155c589fcdc46e6f19fa4a5b
Thomas Hillenbrand <tomhillenbrand16@gmail.com>
2022-01-19 17:55:45 -0600

    add simple tests

--- 01-inbox/test/Inbox.test.js
+++ 01-inbox/test/Inbox.test.js
@@ -6,22 +6,32 @@ const { interface, bytecode } = require('../compile'); // getting these values o
 
 let accounts;
 let inbox;
+const defaultMessage = "Default Message";
 
 beforeEach(async () => {
     // get a list of all accounts
     accounts = await web3.eth.getAccounts();
 
-    console.log(accounts);
-
     // use one of those accounts to deploy the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['Hi There!'] })
+        .deploy({ data: bytecode, arguments: [defaultMessage] })
         .send({ from: accounts[0], gas: '1000000' })
 
 });
 
 describe('Inbox', () => {
     it('deploys a contract', () => {
-        console.log(inbox);
+        assert.ok(inbox.options.address);
+    });
+
+    it('sets a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, defaultMessage);
+    });
+
+    it('updates message with setMessage method', async () => {
+        await inbox.methods.setMessage("new message").send({ from: accounts[0] });
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'new message');
     });
 });
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# learn-blockchain
+# Learn Blockchain
+The goal of this project is to gain a deeper understanding of blockchain technology and its applications in software engineering.
