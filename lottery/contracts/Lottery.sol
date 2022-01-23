0bdc1d64f238a34fdce083988073e45ebf131911
Thomas Hillenbrand <tomhillenbrand16@gmail.com>
2022-01-23 17:00:42 -0600

    initial commit of lottery project

---
+++ lottery/contracts/Lottery.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+
+ contract Lottery {
+    address public manager;
+    address[] public players;
+
+    constructor () {
+        manager = msg.sender; // msg is a global variable always available
+    }
+
+    function enter() public payable {
+        require(msg.value >= 1 ether);
+        players.push(msg.sender);
+    }
+
+    function pickWinner() public restricted {
+        address winnerAddress = players[random() % players.length];
+        payable(winnerAddress).transfer(address(this).balance);
+        players = new address[](0);
+    }
+
+    function random() private view returns(uint){
+       return uint(keccak256(abi.encode(block.difficulty, block.timestamp, players))); // block is global variable
+    }
+
+    function getPlayers() public view returns (address[] memory){
+        return players;
+    }
+
+    modifier restricted() {
+        require(msg.sender == manager);
+        _;
+    }
+
+ }
---
+++ lottery/deploy.js
@@ -0,0 +1,24 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { abi, evm } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'surge middle ticket track renew old rice rib deal symbol today include',
+    'https://rinkeby.infura.io/v3/6495c7741ec34a90aa51087cd888283c'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts(); // provider auto generates account addresses bsed on mnemonic
+
+    console.log('Attempting to deploy from account:', accounts[0]);
+    const result = await new web3.eth.Contract(abi) // grabbed form the compile.js file...
+        .deploy({ data: evm.bytecode.object, arguments: ['Hi there!'] })
+        .send({ gas: '1000000', from: accounts[0] });
+
+    console.log('Contract deployed to: ', result.options.address);
+    provider.engine.stop();
+};
+
+deploy();
+return;
